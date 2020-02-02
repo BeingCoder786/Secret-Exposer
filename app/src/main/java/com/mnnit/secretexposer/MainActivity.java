dc2c05af5b1201d363324774b1e4fda5b0e5728e
Your Name <monu-123>
2020-02-02 15:09:09 +0530

    Initiliase repo

---
+++ app/src/main/java/com/mnnit/secretexposer/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mnnit.secretexposer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+    public void changeToSignupActivity(View view) {
+        Intent intent=new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+}
